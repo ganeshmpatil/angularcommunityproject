63fc3e83f88d49210ae23adc243b8a4a00a5a895
Ganesh Patil <ganesh.patil@oracle.com>
2020-10-03 14:03:06 +0530

    changes for showing blood details.

--- src/app/blood/blood-home/blood-home.component.ts
+++ src/app/blood/blood-home/blood-home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { BloodService } from '../blood.service';
 @Component({
   selector: 'app-blood-home',
   templateUrl: './blood-home.component.html',
-  styleUrls: ['./blood-home.component.css']
+  styleUrls: ['./blood-home.component.css'],
 })
 export class BloodHomeComponent implements OnInit {
-
-  constructor() { }
+  allUsersBloodDetails: any;
+  constructor(private bloodService: BloodService) {}
 
   ngOnInit(): void {
+    this.bloodService.getBloodDetails().subscribe((response: any) => {
+      this.allUsersBloodDetails = response;
+      console.log(JSON.stringify(this.allUsersBloodDetails));
+    });
   }
-
 }
